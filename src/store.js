d56255f5bfed8b7993f6d5886e39077514c68d97
Sapana Gupta <146055257+sapanagupta25@users.noreply.github.com>
2025-05-25 22:30:23 +0530

    Delete src/store.js

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import booksReducer from "./booksReducer";
-import { applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-
-const store = createStore(booksReducer, applyMiddleware(thunk));
-export default store;
